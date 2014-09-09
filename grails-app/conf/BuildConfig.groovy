68f2eafef8bdeb520529c2402eb7f3e7e483536c
ryanv78665 <superscooby7>
2014-09-09 09:34:29 -0500

    upgrade to grails 2.3.9 and terracotta 3.7.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,6 +4,7 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
+grails.project.dependency.resolver = "maven"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
@@ -35,21 +36,27 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.16'
-        compile 'net.sf.ehcache:ehcache-core:2.3.2'
+        /*compile 'net.sf.ehcache:ehcache-core:2.3.2'
         compile 'org.terracotta:terracotta-toolkit-1.1-runtime:2.1.0'
-        compile 'org.terracotta.quartz:quartz-terracotta:1.2.1'
+        compile 'org.terracotta.quartz:quartz-terracotta:1.2.1'*/
+        runtime 'net.sf.ehcache:ehcache-core:2.6.6'
+        runtime 'net.sf.ehcache:ehcache-terracotta:2.6.6'
+        runtime 'org.terracotta:terracotta-toolkit-1.6-runtime:5.5.0'
+
+        runtime 'org.terracotta.quartz:quartz-terracotta:2.3.5'
     }
 
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
-
+        compile ':quartz:1.0-RC7'
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
-
-        build ":tomcat:$grailsVersion"
+        runtime ':hibernate:3.6.10.2'
+        compile ':scaffolding:1.0.0'
+        build ':tomcat:7.0.42'
     }
 }
